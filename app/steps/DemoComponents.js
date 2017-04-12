4420c9b5bda221c57ae85b7e2ca4bc217de3c138
Deepak Grover <deepakgrover333@gmail.com>
2017-04-13 03:11:21 +0530

    Fix syntax highlighting in DemoComponents

--- app/steps/DemoComponents.js
+++ app/steps/DemoComponents.js
@@ -1,48 +1,52 @@
 import React, { Component } from "react";
-import Remarkable from "react-remarkable";
 import { dataOperation } from "../service/DataOperation";
 import { data } from "../service/indexData";
+import renderHTML from "react-render-html";
 
-const base = `
-	<ReactiveBase
-		app="hn"
-		credentials="YOzeIAmyn:f1955c6b-03e7-4eb8-90ca-bfcc28a0ba0c"
-		type="post"
-		theme="rbc-orange"
-	>
-	</ReactiveBase>
+const ReactivebaseMarkup = `
+<pre style='color:#000000;background:#ffffff;'><span style='color:#a65700; '>&lt;</span><span style='color:#5f5035; '>ReactiveBase</span>
+<span style='color:#274796; '>	</span><span style='color:#5f5035; '>app</span><span style='color:#808030; '>=</span><span style='color:#0000e6; '>"hn"</span><span style='color:#274796; '></span>
+<span style='color:#274796; '>	credentials</span><span style='color:#808030; '>=</span><span style='color:#0000e6; '>"YOzeIAmyn:f1955c6b-03e7-4eb8-90ca-bfcc28a0ba0c"</span><span style='color:#274796; '></span>
+<span style='color:#274796; '>	</span><span style='color:#074726; '>type</span><span style='color:#808030; '>=</span><span style='color:#0000e6; '>"post"</span><span style='color:#274796; '></span>
+<span style='color:#274796; '>	theme</span><span style='color:#808030; '>=</span><span style='color:#0000e6; '>"rbc-orange"</span><span style='color:#274796; '></span>
+<span style='color:#a65700; '>></span>
+<span style='color:#a65700; '>&lt;/</span><span style='color:#5f5035; '>ReactiveBase</span><span style='color:#a65700; '>></span>
+</pre>
 `;
 
-const SingleDropdownList = `
-	<SingleDropdownList
-		componentId="TypeSensor"
-		appbaseField="p_type"
-		size={100}
-		selectAllLabel="All"
-		defaultSelected="All"
-	/>
+const SingleDropdownListMarkup = `
+<pre style='color:#000000;background:#ffffff;'><span style='color:#a65700; '>&lt;</span><span style='color:#5f5035; '>SingleDropdownList</span>
+<span style='color:#274796; '>&#xa0;</span><span style='color:#274796; '>   componentId</span><span style='color:#808030; '>=</span><span style='color:#0000e6; '>"TypeSensor"</span><span style='color:#274796; '></span>
+<span style='color:#274796; '>&#xa0;&#xa0;&#xa0;&#xa0;appbaseField</span><span style='color:#808030; '>=</span><span style='color:#0000e6; '>"p_type"</span><span style='color:#274796; '></span>
+<span style='color:#274796; '>&#xa0;&#xa0;&#xa0;&#xa0;</span><span style='color:#074726; '>size</span><span style='color:#808030; '>=</span><span style='color:#274796; '>{</span><span style='color:#008c00; '>100</span><span style='color:#274796; '>}</span>
+<span style='color:#274796; '>&#xa0;&#xa0;&#xa0;&#xa0;selectAllLabel</span><span style='color:#808030; '>=</span><span style='color:#0000e6; '>"All"</span><span style='color:#274796; '></span>
+<span style='color:#274796; '>&#xa0;&#xa0;&#xa0;&#xa0;defaultSelected</span><span style='color:#808030; '>=</span><span style='color:#0000e6; '>"All"</span><span style='color:#274796; '></span>
+<span style='color:#a65700; '>/></span>
+</pre>
 `;
 
-const DataSearch = `
-	<DataSearch
-		componentId="InputSensor"
-		appbaseField={["title", "text", "by"]}
-		placeholder="Search posts by title, text or author..."
-		autocomplete={false}
-	/>
+const DataSearchMarkup = `
+<pre style='color:#000000;background:#ffffff;'><span style='color:#a65700; '>&lt;</span><span style='color:#5f5035; '>DataSearch</span>
+<span style='color:#274796; '>	</span><span style='color:#5f5035; '>componentId</span><span style='color:#808030; '>=</span><span style='color:#0000e6; '>"InputSensor"</span><span style='color:#274796; '></span>
+<span style='color:#274796; '>	appbaseField</span><span style='color:#808030; '>=</span><span style='color:#274796; '>{[</span><span style='color:#0000e6; '>"title"</span><span style='color:#274796; '>, </span><span style='color:#0000e6; '>"text"</span><span style='color:#274796; '>, </span><span style='color:#0000e6; '>"by"</span><span style='color:#274796; '>]}</span>
+<span style='color:#274796; '>	</span><span style='color:#074726; '>placeholder</span><span style='color:#808030; '>=</span><span style='color:#0000e6; '>"Search posts by title, text or author..."</span><span style='color:#274796; '></span>
+<span style='color:#274796; '>	</span><span style='color:#074726; '>autocomplete</span><span style='color:#808030; '>=</span><span style='color:#274796; '>{false}</span>
+<span style='color:#a65700; '>/></span>
+</pre>
 `;
 
-const ResultList = `
-	<ResultList
-		appbaseField="title"
-		from={0}
-		size={20}
-		pagination={true}
-		onData={this.onData}
-		react={{
-			and: ["InputSensor", "TypeSensor"]
-		}}
-	/>
+const ResultListMarkup = `
+<pre style='color:#000000;background:#ffffff;'><span style='color:#a65700; '>&lt;</span><span style='color:#5f5035; '>ResultList</span>
+<span style='color:#274796; '>	</span><span style='color:#5f5035; '>appbaseField</span><span style='color:#808030; '>=</span><span style='color:#0000e6; '>"title"</span><span style='color:#274796; '></span>
+<span style='color:#274796; '>	from</span><span style='color:#808030; '>=</span><span style='color:#274796; '>{</span><span style='color:#008c00; '>0</span><span style='color:#274796; '>}</span>
+<span style='color:#274796; '>	</span><span style='color:#074726; '>size</span><span style='color:#808030; '>=</span><span style='color:#274796; '>{</span><span style='color:#008c00; '>20</span><span style='color:#274796; '>}</span>
+<span style='color:#274796; '>	pagination</span><span style='color:#808030; '>=</span><span style='color:#274796; '>{true}</span>
+<span style='color:#274796; '>	onData</span><span style='color:#808030; '>=</span><span style='color:#274796; '>{this</span><span style='color:#008c00; '>.</span><span style='color:#274796; '>onData}</span>
+<span style='color:#274796; '>	react</span><span style='color:#808030; '>=</span><span style='color:#274796; '>{{</span>
+<span style='color:#274796; '>		and: [</span><span style='color:#0000e6; '>"InputSensor"</span><span style='color:#274796; '>, </span><span style='color:#0000e6; '>"TypeSensor"</span><span style='color:#274796; '>]</span>
+<span style='color:#274796; '>	}}</span>
+<span style='color:#a65700; '>/></span>
+</pre>
 `;
 
 export default class DemoComponents extends Component {
@@ -78,45 +82,45 @@ export default class DemoComponents extends Component {
 	render() {
 		return (
 			<section className="single-step">
-				<h2>Get started with our data-aware components</h2>
+				<h2>Let us assemble all the components</h2>
 
 				<div className="docs-wrapper">
 					<div className="row">
 						<div className="left-col">
-							<Remarkable source={base}></Remarkable>
+							{renderHTML(ReactivebaseMarkup)}
 						</div>
 						<div className="right-col">
-							<p>ReactiveBase is the base component for all ReactiveMaps and ReactiveSearch apps. It binds the backend app (data source) with the UI view components (child elements to ReactiveBase), allowing the UI views to be reactively updated every time there is a change in the data source or in the UI view components.</p>
+							<p>ReactiveBase is the first component in any ReactiveSearch (or Maps) app, it connects the UI layer with the appbase.io data backend.</p>
 						</div>
 					</div>
 
 					<div className="row">
 						<div className="left-col">
-							<Remarkable source={SingleDropdownList}></Remarkable>
+							{renderHTML(SingleDropdownListMarkup)}
 						</div>
 						<div className="right-col">
 							<img src="https://i.imgur.com/kquzzEU.png" />
-							<p>A SingleDropdownList sensor component creates a radio select list UI widget. It is used for filtering results based on the current selection from a list of items.</p>
+							<p><strong>SingleDropdownList</strong> is a dropdown UI component. We will use this to create a search filter by stories, comments, etc. You can read more about it in the docs <a href="https://opensource.appbase.io/reactive-manual/v1.0.0/components/SingleDropdownList.html" target="_blank">here</a>.</p>
 						</div>
 					</div>
 
 					<div className="row">
 						<div className="left-col">
-							<Remarkable source={DataSearch}></Remarkable>
+							{renderHTML(DataSearchMarkup)}
 						</div>
 						<div className="right-col">
 							<img className="big" src="https://i.imgur.com/AxAJkte.png" />
-							<p>A DataSearch sensor component creates a searchbox UI widget with an autocomplete and keyword highlighting functionality. It is used for applying full-text search across one or more fields.</p>
+							<p><strong>DataSearch</strong> is a searchbox UI component that can search on one or more fields, and comes with optional auto-complete suggestions and result highlighting support. You can read more about it in the docs <a href="https://opensource.appbase.io/reactive-manual/v1.0.0/components/DataSearch.html" target="_blank">here</a>.</p>
 						</div>
 					</div>
 
 					<div className="row">
 						<div className="left-col">
-							<Remarkable source={ResultList}></Remarkable>
+							{renderHTML(ResultListMarkup)}
 						</div>
 						<div className="right-col">
 							<img className="big" src="https://i.imgur.com/l0wLoZ4.png" />
-							<p>A ResultList is an actuator component that creates a result list UI widget where results from all the applied filters are shown.</p>
+							<p><strong>ResultList</strong> allows us the search matches in a list format, it supports pagination, infinite scrolling as well as sorting options. You can read more about it in the docs <a href="https://opensource.appbase.io/reactive-manual/v1.0.0/components/ReactiveList.html" target="_blank">here</a>.</p>
 						</div>
 					</div>
 				</div>
--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,10 @@ module.exports = {
 				test: /\.scss$/,
 				loaders: ["style-loader", "css-loader", "sass-loader"]
 			},
+			{
+				test: /\.json$/,
+				loader: 'json'
+			},
 			{ test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
 			{ test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
 		],
